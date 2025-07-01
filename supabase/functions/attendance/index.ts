5f88067db5afbef080682523904828552d6d2dae
δυς <d+gh@dhappy.org>
2025-07-01 03:08:13 -0400

    movin’ config to `.env` & hostin’ API 🐐

--- supabase/functions/attendance/index.ts
+++ supabase/functions/attendance/index.ts
@@ -1,33 +1,36 @@
-// Setup type definitions for built-in Supabase Runtime APIs
-import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import { createClient } from 'jsr:@supabase/supabase-js@2';
-Deno.serve(async (req)=>{
+import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
+import { createClient } from 'jsr:@supabase/supabase-js@2'
+
+Deno.serve(async (_req: Request)=>{
   try {
-    const supabase = createClient(Deno.env.get('SUPABASE_URL') ?? '', Deno.env.get('SUPABASE_ANON_KEY') ?? '', {
-      global: {
-        headers: {
-          Authorization: req.headers.get('Authorization')
-        }
-      }
-    });
-    const { data, error } = await supabase.from('attendance').select('*');
-    if (error) throw error;
+    const supabase = createClient(
+      Deno.env.get('SUPABASE_URL') ?? (
+        (() => { throw new Error(
+          '`SUPABASE_URL` is undefined.'
+        ) })()
+      ),
+      Deno.env.get('ATTENDANCE_ACCESS_KEY') ?? (
+        (() => { throw new Error(
+          '`ATTENDANCE_ACCESS_KEY` is undefined.'
+        ) })()
+      ),
+    )
+    const { data, error } = (
+      await supabase.from('attendance').select('*')
+    )
+    if (error) throw error
     return new Response(JSON.stringify({
       data
     }), {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      status: 200
-    });
+      headers: { 'Content-Type': 'application/json' },
+      status: 200,
+    })
   } catch (err) {
     return new Response(JSON.stringify({
-      message: err?.message ?? err
+      message: (err as Error)?.message ?? err,
     }), {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      status: 500
-    });
+      headers: { 'Content-Type': 'application/json' },
+      status: 500,
+    })
   }
-});
+})
